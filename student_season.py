9ad37db332429bd55fb85bf0b9b3098c39259933
GabrielleRab <32782626+GabrielleRab@users.noreply.github.com>
2019-09-24 17:33:32 -0400

    Delete student_season.py

--- student_season.py
+++
@@ -1,6 +0,0 @@
-#This function takes in a student name and returns their favorite season.
-import pandas as pd
-df = pd.read_csv("internship_bootcamp_data.csv")
-def student_season(name):
-	season = df["Fav season"].loc[df["First Name"] == name]
-	return season
