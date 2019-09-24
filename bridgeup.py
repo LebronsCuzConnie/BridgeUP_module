1acdf4c29caaee10a54aae64805092ff1b56bfff
Sainy <sainyrodriguez0306@gmail.com>
2019-09-24 17:22:36 -0400

    Ice cream_Fla function

--- bridgeup.py
+++ bridgeup.py
@@ -6,3 +6,13 @@
 import pandas as pd
 
 df = pd.read_csv("internship_bootcamp_data.csv")
+ #It loops throught the data in the file and looks for the ice cream flavor 
+ #Gives you how many people has that flavor
+def flavor_num(stu_fla):
+	flavor = df["Fav ice cream flavor"]
+	fla_re = 0
+	for i in flavor:
+		if i == stu_fla:
+			fla_re +=1
+	return fla_re
+print(flavor_num("coffee"))
