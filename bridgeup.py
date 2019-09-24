76bdcd07833b44b454ce4676f3f4ca14bb4b617c
Sabrina <chansabrina0114@gmail.com>
2019-09-24 17:32:11 -0400

    Created season function

--- bridgeup.py
+++ bridgeup.py
@@ -6,3 +6,9 @@
 import pandas as pd
 
 df = pd.read_csv("internship_bootcamp_data.csv")
+
+#This function takes in a student name and returns their favorite season.
+
+def student_season(name):
+	season = df["Fav season"].loc[df["First Name"] == name]
+	return season
