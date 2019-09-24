595492e0ce44cd779f3253da21e4d31c47b434ca
Aleenah <xpeditioner@gmail.com>
2019-09-24 16:48:51 -0400

    saving function

--- bridgeup.py
+++ bridgeup.py
@@ -6,3 +6,13 @@
 import pandas as pd
 
 df = pd.read_csv("internship_bootcamp_data.csv")
+
+def grade_num(num): 
+    grade = df["Grade"]
+    total = 0 
+    for i in grade: 
+        if i == num: 
+            total += 1 
+    return total
+
+# this function counts the amount of brown scholars in a certain grade. 
